c56c2b8a53f6b5dac01e4c035331d0680a85c9db
Alex Baranov <73061540+SaltyByte@users.noreply.github.com>
2022-01-06 12:15:52 +0200

    Organizing the folders

---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./MainPage.css";
+
+const MainPage = () => {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+};
+
+export default MainPage;
